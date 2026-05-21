3b1a8b19ab7c5cc7cee661c59f094ce9236430cc
marcomrillo <marcomurillogom@gmail.com>
2026-05-21 17:58:08 -0500

    test: aplicando mock para simular la libreria de awsglue

--- BigDataTerraform/terraform/archivos_para_bronze/tests/test_etl.py
+++ BigDataTerraform/terraform/archivos_para_bronze/tests/test_etl.py
@@ -1,3 +1,24 @@
+import sys
+from unittest.mock import MagicMock
+
+# ====================================================================
+# TRUCO MOCK: Simulamos la librería de AWS Glue antes de importar el script
+# ====================================================================
+class MockAwsGlueUtils:
+    @staticmethod
+    def getResolvedOptions(args, options):
+        # Retorna un diccionario simulado con los argumentos que pida tu script
+        return {"JOB_NAME": "test_glue_job"}
+
+# Creamos módulos falsos en el sistema de Python
+sys.modules['awsglue'] = MagicMock()
+sys.modules['awsglue.utils'] = MockAwsGlueUtils
+sys.modules['awsglue.context'] = MagicMock()
+sys.modules['awsglue.job'] = MagicMock()
+
+# ====================================================================
+# AHORA SÍ PROSEGUIMOS CON LOS IMPORTS Y TESTS NORMALES
+# ====================================================================
 import pytest
 from pyspark.sql import SparkSession
 from scripts.etl_sales import transform
@@ -10,12 +31,9 @@ def spark():
         .getOrCreate()
 
 def test_transform_schema(spark):
-    # Pasamos una lista de strings simulando lo que va a explotar el script
     data = [
         ("1", "101", "laptop", "1200", "Bogota", "2024-01-01", ["item1", "item2"])
     ]
-    
-    # IMPORTANTE: "datos" DEBE estar aquí listado al final
     columns = ["order_id", "customer_id", "product", "amount", "city", "date", "datos"]
     
     df = spark.createDataFrame(data, columns)
@@ -27,8 +45,6 @@ def test_transform_values(spark):
     data = [
         ("1", "101", "laptop", "1200", "Bogota", "2024-01-01", ["item1"])
     ]
-    
-    # IMPORTANTE: Aquí también sumamos "datos"
     columns = ["order_id", "customer_id", "product", "amount", "city", "date", "datos"]
     
     df = spark.createDataFrame(data, columns)
