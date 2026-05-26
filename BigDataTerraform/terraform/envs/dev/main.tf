d5d2a51c882c97b1ea01998b695b1531bd1af7bc
marcomrillo <marcomurillogom@gmail.com>
2026-05-26 18:30:11 -0500

    corregido valores redundantes en main

--- BigDataTerraform/terraform/envs/dev/main.tf
+++ BigDataTerraform/terraform/envs/dev/main.tf
@@ -1,14 +1,26 @@
-# =================================================================
-# INFRASTRUCTURE MODULES (BUCKETS) - CONFIGURACIÓN ESTRICTA
-# =================================================================
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region = var.aws_region
+}
+
+# --- Lo que faltaba: Presentamos el data source ---
+data "aws_caller_identity" "current" {}
+# --------------------------------------------------
+
+# INFRASTRUCTURE MODULES (BUCKETS)
 module "raw_bucket" {
   source      = "../../modules/s3_lake"
-  # Variables requeridas por el módulo interno s3_lake
   project     = var.project
   env         = var.env
   account_id  = data.aws_caller_identity.current.account_id
-  # Variable para el nombre del bucket
   bucket_name = "${var.project}-${var.env}-raw-${data.aws_caller_identity.current.account_id}"
   tags        = var.tags
 }
@@ -29,4 +41,31 @@ module "analytics_bucket" {
   account_id  = data.aws_caller_identity.current.account_id
   bucket_name = "${var.project}-${var.env}-gold-${data.aws_caller_identity.current.account_id}"
   tags        = var.tags
+}
+
+# MODULE IAM
+module "iam" {
+  source           = "../../modules/iam"
+  project          = var.project
+  env              = var.env
+  raw_bucket       = module.raw_bucket.bucket_name
+  staging_bucket   = module.staging_bucket.bucket_name
+  analytics_bucket = module.analytics_bucket.bucket_name
+  temp_bucket      = module.raw_bucket.bucket_name
+}
+
+# MODULE GLUE
+module "glue_jobs" {
+  source = "../../modules/glue"
+
+  project         = var.project
+  env             = var.env
+  glue_role_arn   = module.iam.glue_role_arn
+  
+  raw_bucket      = module.raw_bucket.bucket_name
+  staging_bucket  = module.staging_bucket.bucket_name
+  temp_bucket     = module.raw_bucket.bucket_name
+  
+  script_location = "s3://${module.raw_bucket.bucket_name}/scripts/bronze_to_silver.py"
+  tags            = var.tags
 }
